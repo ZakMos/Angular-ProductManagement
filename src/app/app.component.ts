b8f925fd1fe9aa3259e6f927d8af051199210ea3
ZakMos <z.zza@hotmail.com>
2019-07-08 21:49:28 +0200

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-ProductManagement';
+  title = 'Angular: Product Management';
 }
